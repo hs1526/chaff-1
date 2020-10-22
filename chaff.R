cf2a66ad54cffd10e9c687670d482ff2f79e0523
DomMunns <dem520@york.ac.uk>
2020-10-22 16:01:46 +0100

    imported raw data tidied raw data into a new dataframe summarised tidied data analysed data using a one-way anova

---
+++ chaff.R
@@ -0,0 +1,22 @@
+#import dataset
+chaff <- read.table("raw_data/chaff.txt", header = TRUE)
+
+#tidy data
+library(tidyverse)
+chaff2 <- gather(chaff, key = sex, mass)
+  
+#summarise data in a new dataframe
+chaff_sum <- chaff2 %>% 
+  group_by(sex) %>% 
+  summarise(mean = mean(mass),
+            n = length(mass),
+            std= sd(mass),
+            se = std/sqrt(n))
+
+#analyse data using an anova
+anova <- aov(mass ~ sex, data = chaff2)
+summary(anova)
+#there is a significant difference in mass between sexes
+
+
+
